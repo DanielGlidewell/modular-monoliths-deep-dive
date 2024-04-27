132a0a02198b8d46f5eb7216e50fba0aad5869ed
Danny Glidewell <daniel.glidewell@outlook.com>
2024-04-27 17:26:57 -0400

    implement the enhanced book service interface with basic functionality

--- src/RiverBooks.Books/BookService.cs
+++ src/RiverBooks.Books/BookService.cs
@@ -1,11 +1,56 @@
+
 namespace RiverBooks.Books;
 
 internal class BookService : IBookService {
-  public List<BookDto> ListBooks() {
-    return [
-      new BookDto(Guid.NewGuid(), "The Fellowship of the Ring", "J.R.R. Tolkien"),
-      new BookDto(Guid.NewGuid(), "The Two Towers", "J.R.R. Tolkien"),
-      new BookDto(Guid.NewGuid(), "The Return of the King", "J.R.R. Tolkien")
-    ];
+  private readonly IBookRepository _bookRepository;
+
+  public BookService(IBookRepository bookRepository) {
+    _bookRepository = bookRepository;
+  }
+
+  public async Task CreateBookAsync(BookDto newBook) {
+    var book = new Book(newBook.Id, newBook.Title, newBook.Author, newBook.Price);
+    await _bookRepository.AddAsync(book);
+    await _bookRepository.SaveChangesAsync();
+  }
+
+  public async Task DeleteBookAsync(Guid bookId)
+  {
+    var bookToDelete = await _bookRepository.GetByIdAsync(bookId);
+    if (bookToDelete is not null) {
+      await _bookRepository.DeleteAsync(bookToDelete);
+      await _bookRepository.SaveChangesAsync();
+    }
+
+    // throw exception if book not found
+  }
+
+  public async Task<BookDto> GetBookByIdAsync(Guid bookId)
+  {
+    var book = await _bookRepository.GetByIdAsync(bookId);
+
+    // handle book not found 
+
+    return new BookDto(book!.Id, book.Title, book.Author, book.Price); 
+  }
+
+  public async Task<List<BookDto>> ListBooksAsync() {
+    var books = (await _bookRepository.ListAsync())
+      .Select(book => new BookDto(book.Id, book.Title, book.Author, book.Price))
+      .ToList();
+
+    return books;
+  }
+
+  public async Task UpdateBookPriceAsync(Guid bookId, decimal newPrice)
+  {
+    // validate the price
+
+    var book = await _bookRepository.GetByIdAsync(bookId);
+
+    // handle book not found
+
+    book!.UpdatePrice(newPrice);
+    await _bookRepository.SaveChangesAsync();
   }
 }
