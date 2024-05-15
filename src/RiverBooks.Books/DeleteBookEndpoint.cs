217c1e301b683020dff73359104a663d8a79740f
Danny Glidewell <daniel.glidewell@outlook.com>
2024-05-15 08:00:42 -0400

    delete book endpoint

---
+++ src/RiverBooks.Books/DeleteBookEndpoint.cs
@@ -0,0 +1,23 @@
+using FastEndpoints;
+
+namespace RiverBooks.Books;
+
+internal class DeleteBookEndpoint(IBookService bs) :
+  Endpoint<DeleteBookRequest> {
+
+  private readonly IBookService bookService = bs;
+
+  public override void Configure() {
+    Delete("/books/{Id}");
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(
+    DeleteBookRequest request,
+    CancellationToken ct = default
+  ) {
+    await bookService.DeleteBookAsync(request.Id);
+
+    await SendNoContentAsync(cancellation: ct);
+  }
+}
