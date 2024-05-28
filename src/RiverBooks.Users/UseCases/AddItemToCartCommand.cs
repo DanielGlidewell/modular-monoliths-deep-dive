0348af2f3eff66cf08efdcc558f92cac5b976693
Danny Glidewell <daniel.glidewell@outlook.com>
2024-05-28 14:02:41 -0400

    chore: refactor types into separate files

--- src/RiverBooks.Users/UseCases/AddItemToCartCommand.cs
+++ src/RiverBooks.Users/UseCases/AddItemToCartCommand.cs
@@ -5,31 +5,3 @@ namespace RiverBooks.Users.UseCases;
 
 public record AddItemToCartCommand(Guid BookId, int Quantity, string Email) 
   : IRequest<Result>;
-
-public class AddItemToCartHandler(IApplicationUserRepository userRepository) : IRequestHandler<AddItemToCartCommand, Result>
-{
-
-  public async Task<Result> Handle(AddItemToCartCommand request, CancellationToken cancellationToken)
-  {
-    var user = await userRepository.GetByEmailAsync(request.Email);
-
-    if(user is null)
-    {
-      return Result.Unauthorized();
-    }
-    
-    // TODO: Get description and price from Books module
-    var newCartItem = new CartItem(
-      request.BookId,
-      "description",
-      request.Quantity,
-      1.00m
-    );
-
-    user.AddItemToCart(newCartItem);
-
-    await userRepository.SaveChangesAsync();
-
-    return Result.Success();
-  }
-}
