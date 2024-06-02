45c1ff16a9193ad224b7a2c0f390e24611e47953
Danny Glidewell <daniel.glidewell@outlook.com>
2024-06-02 13:20:17 -0400

    feat: modeling the order and order item entities as well as the address value object

---
+++ src/RiverBooks.OrderProcessing/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace RiverBooks.OrderProcessing;
+
+internal class Order 
+{
+  private readonly List<OrderItem> _orderItems = new(); 
+  public Guid Id { get; private set; } = Guid.NewGuid();
+  public Guid UserId {  get; private set; }
+  public Address ShippingAddress { get; private set; } = default!;
+  public Address BillingAddress { get; private set; } = default!;
+  public IReadOnlyCollection<OrderItem> orderItems => _orderItems.AsReadOnly();
+  public DateTimeOffset DateCreated { get; private set; } = DateTimeOffset.Now;
+
+  private void AddOrderItem(OrderItem item) => _orderItems.Add(item);
+}
