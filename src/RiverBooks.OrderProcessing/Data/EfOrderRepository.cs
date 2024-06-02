d835af0a1446f00b3038b6d4360795fd7647ca62
Danny Glidewell <daniel.glidewell@outlook.com>
2024-06-02 13:23:34 -0400

    feat: implementing simple repo

---
+++ src/RiverBooks.OrderProcessing/Data/EfOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RiverBooks.OrderProcessing.Data;
+
+internal class EfOrderRepository(OrderProcessingDbContext dbContext) : IOrderRepository
+{
+
+  public async Task AddAsync(Order order)
+  {
+    await dbContext.Orders.AddAsync(order);
+  }
+
+  public async Task<List<Order>> ListAsync()
+  {
+    return await dbContext.Orders.ToListAsync();
+  }
+
+  public async Task SaveChangesAsync()
+  {
+    await dbContext.SaveChangesAsync();
+  }
+}
