3008ac52fdb7c7e3942dc27da17be5e1ce3d7fff
Danny Glidewell <daniel.glidewell@outlook.com>
2024-06-02 13:22:18 -0400

    feat: implementing the db context for order processing as well as declaring the configuration for the order table that ef core will use to create the db schema

---
+++ src/RiverBooks.OrderProcessing/Data/OrderConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RiverBooks.OrderProcessing.Data;
+
+internal static class Constants
+{
+  public const int STREET_MAXLENGTH = 50;
+  public const int CITY_MAXLENGTH = 50;
+  public const int STATE_MAXLENGTH = 50;
+  public const int POSTALCODE_MAXLENGTH = 20;
+  public const int COUNTRY_MAXLENGTH = 50;
+}
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+  void IEntityTypeConfiguration<Order>.Configure(EntityTypeBuilder<Order> builder)
+  {
+    builder
+      .Property(o => o.Id)
+      .ValueGeneratedNever();
+
+    builder.ComplexProperty(o => o.ShippingAddress, address =>
+    {
+      address.Property(a => a.Street1)
+        .HasMaxLength(Constants.STREET_MAXLENGTH);
+      address.Property(a => a.Street2)
+        .HasMaxLength(Constants.STREET_MAXLENGTH);
+      address.Property(a => a.City)
+        .HasMaxLength(Constants.CITY_MAXLENGTH);
+      address.Property(a => a.State)
+        .HasMaxLength(Constants.STATE_MAXLENGTH);
+      address.Property(a => a.PostalCode)
+        .HasMaxLength(Constants.POSTALCODE_MAXLENGTH);
+      address.Property(a => a.Country)
+        .HasMaxLength(Constants.COUNTRY_MAXLENGTH);
+    });
+
+    builder.ComplexProperty(o => o.BillingAddress, address =>
+    {
+      address.Property(a => a.Street1)
+        .HasMaxLength(Constants.STREET_MAXLENGTH);
+      address.Property(a => a.Street2)
+        .HasMaxLength(Constants.STREET_MAXLENGTH);
+      address.Property(a => a.City)
+        .HasMaxLength(Constants.CITY_MAXLENGTH);
+      address.Property(a => a.State)
+        .HasMaxLength(Constants.STATE_MAXLENGTH);
+      address.Property(a => a.PostalCode)
+        .HasMaxLength(Constants.POSTALCODE_MAXLENGTH);
+      address.Property(a => a.Country)
+        .HasMaxLength(Constants.COUNTRY_MAXLENGTH);
+    });
+  }
+}
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+  void IEntityTypeConfiguration<OrderItem>.Configure(EntityTypeBuilder<OrderItem> builder)
+  {
+    builder
+      .Property(oi => oi.Id)
+      .ValueGeneratedNever();
+
+    builder.Property(oi => oi.Description)
+      .HasMaxLength(100)
+      .IsRequired();
+  }
+}
---
+++ src/RiverBooks.OrderProcessing/Data/OrderProcessingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace RiverBooks.OrderProcessing.Data;
+
+public class OrderProcessingDbContext(DbContextOptions<OrderProcessingDbContext> options) : DbContext(options)
+{
+  internal DbSet<Order> Orders { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.HasDefaultSchema("OrderProcessing");
+
+    builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    base.OnModelCreating(builder);
+  }
+
+  protected override void ConfigureConventions(ModelConfigurationBuilder configBuilder)
+  {
+    configBuilder.Properties<decimal>().HavePrecision(18, 6); 
+  }
+}
